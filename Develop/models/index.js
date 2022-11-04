c65dd1d2479be0b6b78192c085cb3f7173f0b043
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-04 18:51:37 -0400

    built the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,30 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Products.belongsTo(Category, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE',
+});
+
 // Categories have many Products
+Categories.hasMany(Products, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Products.belongsToMany(Tags, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tags.belongsToMany(Products, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
