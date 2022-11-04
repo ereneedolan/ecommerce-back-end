eaaebbde7a55de7e8b73bd0db985fc743da4fb77
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-04 19:20:36 -0400

    updated tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,84 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
+  try {
+    const tag = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const category = await category.create({
+      reader_id: req.body.reader_id,
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const category = await Category.findOneAndUpdate (req.body,{
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!category) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  router.delete('/:id', async (req, res) => {
+    try {
+      const category = await Category.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!category) {
+        res.status(404).json({ message: 'No category found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(category);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 });
 
 module.exports = router;
